7420feac50104d45146279f8b0d6c40693b30980
habibanesro <habibanesro9040@gmail.com>
2025-04-13 23:15:02 +0300

    Specified Constants for the app

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,6 +2,5 @@ class AppConstants {
   static const String appName = "ShopEase";
   static const String baseUrl = 'https://fakestoreapi.com';
   static const String productsEndpoint = '/products';
-  static const String cartEndpoint = '/carts';
   static const String favoritesKey = 'favorites';
 }
