23e6cc0ee2aa96e4247508aa0eb29977a2705e36
habibanesro <habibanesro9040@gmail.com>
2025-04-14 01:08:22 +0300

    deleted unused imports

--- lib/presentation/screens/cart_screen.dart
+++ lib/presentation/screens/cart_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:shopease/domain/entities/product.dart';
 import 'package:shopease/presentation/bloc/cart/cart_bloc.dart';
 
 class CartScreen extends StatelessWidget {
