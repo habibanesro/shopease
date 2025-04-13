3825d0ff1f8de1963a0c1f9ed99c983cbd23ac79
habibanesro <habibanesro9040@gmail.com>
2025-04-13 23:34:28 +0300

    added dependency injections

---
+++ lib/core/utils/dependency_injection.dart
@@ -0,0 +1,35 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shopease/data/datasources/product_remote_data_source.dart';
+import 'package:shopease/data/repositories/product_repository_impl.dart';
+import 'package:shopease/domain/repositories/product_repository.dart';
+import 'package:shopease/domain/usecases/get_products.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton(() => sharedPreferences);
+  
+  getIt.registerLazySingleton(() => Dio()
+    ..options = BaseOptions(
+      baseUrl: AppConstants.baseUrl,
+      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 10),
+    ));
+
+  // Data sources
+  getIt.registerLazySingleton<ProductRemoteDataSource>(
+    () => ProductRemoteDataSourceImpl(dio: getIt()),
+  );
+
+  // Repositories
+  getIt.registerLazySingleton<ProductRepository>(
+    () => ProductRepositoryImpl(remoteDataSource: getIt()),
+  );
+
+  // Use cases
+  getIt.registerLazySingleton(() => GetProducts(getIt()));
+}
